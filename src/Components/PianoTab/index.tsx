e275aad484b3f4bfb348d8476ac0d4d41e8ce003
Melissa Bylund <mebylund@yahoo.com>
2019-09-05 16:57:47 -0600

    Added Styles and fixed checkAnswers

--- src/Components/PianoTab/index.tsx
+++ src/Components/PianoTab/index.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
-import { Button } from '@material-ui/core';
+import { Button, Theme, makeStyles, createStyles } from '@material-ui/core';
 
 
 interface State {
     input: string;
 }
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      width: 200,
+      color: 'red',
+    },
+    color: {
+        color: 'red',
+    }
+  }),
+);
+
 
 export default function PianoTab() {
+
+    const classes = useStyles();
 
     const [value1, setValue1] = React.useState<State>({ input: '' })
     const [value2, setValue2] = React.useState<State>({ input: '' })
@@ -21,6 +41,9 @@ export default function PianoTab() {
     const [correct, showCorrect] = React.useState(false)
     const [again, tryAgain] = React.useState(false)
     const [none, noneCorrect] = React.useState(false)
+
+    const correctAnswers = 'GAEDFBC';
+    var allInput = value1.input + value2.input + value3.input + value4.input + value5.input + value6.input + value7.input;
 
 
     const handleChange1 = (input: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -46,16 +69,28 @@ export default function PianoTab() {
     };
 
     const checkAnswers = () => {
-        if(value1.input === 'G' || value2.input === 'A' || value3.input === 'E' || value4.input === 'D' || value5.input === 'F' || value6.input === 'B' || value7.input === 'C'){
-            tryAgain(true)
-            noneCorrect(false)
-        }
-        if(value1.input === 'G' && value2.input === 'A' && value3.input === 'E' && value4.input === 'D' && value5.input === 'F' && value6.input === 'B' && value7.input === 'C'){
+        console.log(allInput);
+        var splitAnswers = correctAnswers.split('');
+        var i = 0;
+        var countCorrect = 0;
+        splitAnswers.forEach(e => {
+            if(e === allInput[i]){
+                countCorrect++;
+
+            }
+            i++;
+            if(countCorrect > 0 && countCorrect < 7){
+                tryAgain(true)
+                noneCorrect(false)
+            }
+
+        })
+        if(correctAnswers === allInput){
             tryAgain(false)
             noneCorrect(false)
             showCorrect(true)
         }
-        else{
+        if(countCorrect === 0)  {
             noneCorrect(true)
         }
 
@@ -63,11 +98,12 @@ export default function PianoTab() {
     }
 
     return (
-        <div>
+        <div className={classes.container} >
             <TextField
                 id="filled-name"
                 label="#1"
                 placeholder="Type here"
+                className={classes.textField}
                 value={value1.input}
                 onChange={handleChange1('input')}
                 margin="normal"
@@ -78,6 +114,7 @@ export default function PianoTab() {
                 id="filled-name"
                 label="#2"
                 placeholder="Type here"
+                className={classes.textField}
                 value={value2.input}
                 onChange={handleChange2('input')}
                 margin="normal"
@@ -88,6 +125,7 @@ export default function PianoTab() {
                 id="filled-name"
                 label="#3"
                 placeholder="Type here"
+                className={classes.textField}
                 value={value3.input}
                 onChange={handleChange3('input')}
                 margin="normal"
@@ -98,6 +136,7 @@ export default function PianoTab() {
                 id="filled-name"
                 label="#4"
                 placeholder="Type here"
+                className={classes.textField}
                 value={value4.input}
                 onChange={handleChange4('input')}
                 margin="normal"
@@ -108,6 +147,7 @@ export default function PianoTab() {
                 id="filled-name"
                 label="#5"
                 placeholder="Type here"
+                className={classes.textField}
                 value={value5.input}
                 onChange={handleChange5('input')}
                 margin="normal"
@@ -118,6 +158,7 @@ export default function PianoTab() {
                 id="filled-name"
                 label="#6"
                 placeholder="Type here"
+                className={classes.textField}
                 value={value6.input}
                 onChange={handleChange6('input')}
                 margin="normal"
@@ -128,6 +169,7 @@ export default function PianoTab() {
                 id="filled-name"
                 label="#7"
                 placeholder="Type here"
+                className={classes.textField}
                 value={value7.input}
                 onChange={handleChange7('input')}
                 margin="normal"
